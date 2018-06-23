f3a71b78b79b4bfa3ca4c90b6df57ce81acf9cdd
tyler-vs <tylervucf@knights.ucf.edu>
2018-06-23 03:09:47 -0400

    feat: Add close functionality to side nav
    - add function to prevent links contained within the side nav
      from closing the side nav when it is in an open/visible state.
    - add styles for side nav's header and content components
    - add css styles for scroll capabilities for the sidebar content
    - add css styles for momentum scrolling capabilities via the
      -webkit-overflow-scrolling css property
    - bind the blockClick function to the js-side-nav-container DOM elem, which
      is the side-nav__container
    - NOTE: the js related classes do not use BEM-like naming, instead is
      used a prepended js- to the class, and if being added to an element
      with an existing class then usually will replace BEM characters for
      class name with a simple dash. (i.e. side-nav__container is
      BEM-flavored and used for CSS while js-side-nav-container was based
      off of the original class name but only used for the side nav JS script)

--- js/side-nav.js
+++ js/side-nav.js
@@ -11,9 +11,11 @@ class SideNav {
     this.showButtonEl = document.querySelector('.js-menu-show');
     this.hideButtonEl = document.querySelector('.js-menu-hide');
     this.sideNavEl = document.querySelector('.js-side-nav');
+    this.sideNavContainer = document.querySelector('.js-side-nav-container');
 
     this.showSideNav = this.showSideNav.bind(this);
     this.hideSideNav = this.hideSideNav.bind(this);
+    this.blockClicks = this.blockClicks.bind(this);
 
     this.addEventListeners();
   }
@@ -21,6 +23,18 @@ class SideNav {
   addEventListeners() {
     this.showButtonEl.addEventListener('click', this.showSideNav);
     this.hideButtonEl.addEventListener('click', this.hideSideNav);
+    this.sideNavEl.addEventListener('click', this.hideSideNav);
+    this.sideNavContainer.addEventListener('click', this.blockClicks);
+  }
+
+  /**
+   * Prevent side nav from closing from clicking on the side nav
+   * buttons while in an open state.
+   * @param  {[type]} evt event emitted from clicked element.
+   * @return {[type]}     [description]
+   */
+  blockClicks (evt) {
+    evt.stopPropagation();
   }
 
   showSideNav() {
