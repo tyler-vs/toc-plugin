d91845e77f95b198eef505a5cadd375676b47c4c
tyler-vs <tylervucf@knights.ucf.edu>
2018-06-23 02:42:53 -0400

    feat: Add close button for side nav

--- js/side-nav.js
+++ js/side-nav.js
@@ -8,20 +8,27 @@
 class SideNav {
 
   constructor() {
-    this.toggleButtonEl = document.querySelector('.js-menu');
+    this.showButtonEl = document.querySelector('.js-menu-show');
+    this.hideButtonEl = document.querySelector('.js-menu-hide');
     this.sideNavEl = document.querySelector('.js-side-nav');
 
     this.showSideNav = this.showSideNav.bind(this);
+    this.hideSideNav = this.hideSideNav.bind(this);
 
     this.addEventListeners();
   }
 
   addEventListeners() {
-    this.toggleButtonEl.addEventListener('click', this.showSideNav);
+    this.showButtonEl.addEventListener('click', this.showSideNav);
+    this.hideButtonEl.addEventListener('click', this.hideSideNav);
   }
 
   showSideNav() {
     this.sideNavEl.classList.add('side-nav--visible');
+  }
+
+  hideSideNav() {
+    this.sideNavEl.classList.remove('side-nav--visible');
   }
 }
 
