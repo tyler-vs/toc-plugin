d3db258c08b7b7bbb231aed8120586457e8612b9
tyler-vs <tylervucf@knights.ucf.edu>
2018-08-16 15:50:18 -0400

    feat: update toc-script.js

--- js/toc-script.js
+++ js/toc-script.js
@@ -4,3 +4,26 @@
  * script from Go Make Things email.
  *
  */
+
+;(function (window, document, undefined) {
+
+  'use strict';
+
+  // Get all of the headings
+  var headings = document.querySelectorAll( 'h2, h3, h4, h5, h6' );
+  if ( headings.length < 1 ) return;
+
+  // Create the links
+  var links = '';
+  for ( var i = 0; i < headings.length; i++ ) {
+    links += '<li><a href="#' + headings[i].id + '">' + headings[i].innerHTML + '</a></li>';
+  }
+
+  // Get TOC container
+  var toc = document.querySelector( '[data-toc]' );
+  if ( !toc ) return;
+
+  // Inject TOC into the DOM
+  toc.innerHTML = '<h2>Table of Contents</h2><ul>' + links + '</ul>';
+
+})(window, document);
